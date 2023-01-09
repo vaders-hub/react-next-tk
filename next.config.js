abb26699b413218d8557f6290c247e8b163f9b99
hyunbae.jeon <mazingazz@gmail.com>
2023-01-09 19:30:02 +0900

    next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
