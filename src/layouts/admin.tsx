8ea3103e9fe54c372acdc453aacd4fe8fa968cb3
hyunbae.jeon <mazingazz@gmail.com>
2023-03-05 09:38:33 +0900

    theme

--- src/layouts/admin.tsx
+++ src/layouts/admin.tsx
@@ -1,17 +1,12 @@
 import NavHeader from 'src/components/NavHeader';
-import { darkTheme } from 'theme/themes';
-import { ThemeProvider, CssBaseline } from '@mui/material';
 
 import type { LayoutProps } from 'src/types/layout';
 
 const AdminLayout: LayoutProps = ({ children }) => {
   return (
     <>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <NavHeader />
-        <div>Admin: {children}</div>
-      </ThemeProvider>
+      <NavHeader />
+      <div>Admin: {children}</div>
     </>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import { FC, Fragment } from 'react';
 import { store, wrapper } from 'src/configs/store';
 import { Provider } from 'react-redux';
 
+import 'styles/globals.css';
+import { darkTheme } from 'theme/themes';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+
 import type { NextComponentType } from 'next';
 import type { AppProps } from 'next/app';
-
-import 'styles/globals.css';
 
 type CustomNextComponent = NextComponentType & { Layout?: FC };
 type CustomAppProps = AppProps & { Component: CustomNextComponent };
@@ -16,9 +18,12 @@ const MyApp = ({ Component, ...rest }: CustomAppProps) => {
   const Layout: CustomNextComponent | typeof Fragment = Component.Layout ? Component.Layout : Fragment;
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...props.pageProps} />
-      </Layout>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Layout>
+          <Component {...props.pageProps} />
+        </Layout>
+      </ThemeProvider>
     </Provider>
   );
 };
